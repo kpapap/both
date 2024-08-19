81737d1efe3568199e5043a9bf445f6726118de5
konstantinos <kostas@norbloc.com>
2024-08-19 13:43:33 +0300

    Update files to include both code

---
+++ factory.go
@@ -0,0 +1,59 @@
+package both
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/receiver"
+)
+
+const (defaultInterval = "1m")
+var (typeStr = component.MustNewType("both"))
+const (defaultNbcmrInterval = "1m")
+const (defaultConfigMapName = "nbcmr-cm")
+
+// NewFactory creates a new receiver factory.
+func NewFactory() receiver.Factory {
+	return receiver.NewFactory(
+		typeStr,
+		createWhitelistDefaultConfig,
+		createNbcmrDefaultConfig,
+		receiver.WithLogs(createWhitelistLogsReceiver, component.StabilityLevelUndefined),
+		receiver.WithLogs(createNbcmrLogsReceiver, component.StabilityLevelUndefined),
+	)
+}
+
+// createDefaultConfig returns the default configuration for the  receiver.
+// This function is used when creating a new factory to provide a default configuration
+// for the receiver.
+func createWhitelistDefaultConfig() component.Config {
+	return &Config{
+		Interval: defaultInterval,
+	}
+}
+
+// createLogsReceiver creates a new instance of the logs receiver.
+// createLogsReceiver creates a log receiver based on provided config.
+func createWhitelistLogsReceiver(_ context.Context, settings receiver.Settings, cfg component.Config, consumer consumer.Logs) (receiver.Logs, error) {
+	// Create the new receiver
+	rCfg := cfg.(*Config)
+	return newWhitelistReceiver(rCfg, consumer, settings)
+}
+
+// createLogsReceiver creates a new instance of the nbcmr receiver.
+func createNbcmrLogsReceiver(_ context.Context, settings receiver.Settings, cfg component.Config, consumer consumer.Logs) (receiver.Logs, error) {
+	// Create the new receiver
+	rCfg := cfg.(*Config)
+	return newNbcmrReceiver(rCfg, consumer, settings)
+}
+
+// createDefaultConfig returns the default configuration for the nbcmr receiver.
+func createNbcmrDefaultConfig() component.Config {
+	return &Config{
+		Interval: defaultInterval,
+		ConfigMapName: defaultConfigMapName,
+	}
+}
+
+
